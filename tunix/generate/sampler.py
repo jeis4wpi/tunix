3825bc970cb7f39f8b75d7899ecae9d2b5af99ad
A9isha <mazumdera@google.com>
2025-08-08 01:01:18 +0000

    updates for grpo_trainer

--- tunix/generate/sampler.py
+++ tunix/generate/sampler.py
@@ -721,6 +721,7 @@ class Sampler(base_sampler.BaseSampler):
         self._flattened_transformer_state, sampling_state
     )
     token_buffers = sampling_state.token_buffer
+    print("token_buffers={token_buffers}")
     logits_buffers = sampling_state.logits_buffer
 
     if sampling_state.sampling_mode == 'beam_search':
